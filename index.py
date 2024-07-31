dac962d4cd07cde7654b9e1b2a972311c6492d4e
Maija Laine <maijaelina05@gmail.com>
2024-07-31 18:45:23 +0300

    Solution

--- index.py
+++ index.py
@@ -1,12 +1,36 @@
-# Password generator
+import string
+import random
 
-# Get random character
-## Letter, Number, Special character
-### For letters randomly lower/uppercase
+def get_random_letter():
+    letter = random.choice(string.ascii_letters)
+    return letter
 
-# password string variable
+def get_random_number():
+    number = random.randint(0,9)
+    return str(number)
 
-# Add/Append to string variable a random character specific number of times
+def get_random_special_char():
+    special_chars = ["!", "'", "#", "$", "%", "&", "+", "=", "*", "-", "_"]
+    special_char = special_chars[random.randint(0, len(special_chars)-1)]
+    return special_char
 
+def get_random_char():
+    char_type = random.randint(0,2)
+    if(char_type == 0):
+        return get_random_letter()
+    if(char_type == 1):
+        return get_random_number()
+    if(char_type == 2):
+        return get_random_special_char()
+    
+def generate(length): 
+    password = ""
 
-# Further development: check criteria for password
+    i = 1
+    while i < length+1:
+        password += get_random_char()
+        i += 1
+
+    return password
+
+print(generate(12))
