39c970d8d0f7f0f3314f4f82252721f9e36214e7
Maija Laine <maijaelina05@gmail.com>
2024-07-31 18:10:08 +0300

    Plan

---
+++ index.py
@@ -0,0 +1,12 @@
+# Password generator
+
+# Get random character
+## Letter, Number, Special character
+### For letters randomly lower/uppercase
+
+# password string variable
+
+# Add/Append to string variable a random character specific number of times
+
+
+# Further development: check criteria for password
